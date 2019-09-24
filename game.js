37fc7b3cc9786df37b7f19920be099a291f03e9a
Jonathan Hillman <jonathandalehillman@gmail.com>
2019-09-23 20:26:14 -0700

    Add files via upload

--- game.js
+++ game.js
@@ -5,8 +5,7 @@ gameport.appendChild(renderer.view);
 
 var stage = new PIXI.Container();
 
-// define sprites
-
+//define sprites
 var bird_nest = new PIXI.Sprite(PIXI.Texture.fromImage("bird_nest.png"))
 
 var bird = new PIXI.Sprite(PIXI.Texture.fromImage("bird.png"));
@@ -17,8 +16,11 @@ var victoryScreen = new PIXI.Sprite(PIXI.Texture.fromImage("victory_screen.png")
 
 var defeatScreen = new PIXI.Sprite(PIXI.Texture.fromImage("defeat_screen.png"));
 
-//set position of all the sprites
+bird.anchor.x = 0.5;
+bird.anchor.y = 0.5;
 
+
+//organize scene graph stuff
 bird.position.x = 100;
 bird.position.y = 300;
 
@@ -37,48 +39,49 @@ stage.addChild(bird);
 stage.addChild(bird_nest);
 stage.addChild(hunter);
 
+//set up WASD
 function keydownEventHandler(e)
 {
-	if(e.keyCode == 87)// W key
+	if(e.keyCode == 87)
 	{
 		bird.position.y -= 10;
 	}
-	if(e.keyCode == 83)// S key
+	if(e.keyCode == 83)
 	{
 		bird.position.y += 10;
 	}
-	if(e.keyCode == 65)// D key
+	if(e.keyCode == 65)
 	{
 		bird.position.x -= 10;
 	}
-	if(e.keyCode == 68)// A key
+	if(e.keyCode == 68)
 	{
 		bird.position.x += 10;
 	}
 }
 document.addEventListener('keydown', keydownEventHandler);
 
+//rules for the game
 function animate() {
 	requestAnimationFrame(animate);
 	renderer.render(stage);
-	
-	//bird is above hunter
+	//if bird is above hunter
 	if(bird.position.y-10 < hunter.position.y)
 	{
 		hunter.position.y -= 2;
 	}
-	//bird is below hunter
+	//if bird is below hunter
 	if(bird.position.y-10 > hunter.position.y)
 	{
 		hunter.position.y += 2;
 	}
-	//victory
+	//if bird is in nest
 	if(bird.position.x - 10 == bird_nest.position.x && bird.position.y == bird_nest.position.y+10)
 	{
 		stage.addChild(victoryScreen);
 		document.removeEventListener('keydown', keydownEventHandler);
 	}
-	//defeat
+	//if bird is caught
 	if(bird.position.x-20 == hunter.position.x && bird.position.y-10 == hunter.position.y)
 	{
 		stage.addChild(defeatScreen);
